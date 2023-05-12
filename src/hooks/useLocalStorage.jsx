91f99dec8fcdb2bebf117a3c50328ae1d6eaf523
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-05-11 21:37:17 -0500

    :bug: Fixed issue related null value on localStorage key

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
+import { defaultTodos } from '../data/defaultTodos';
 
 function useLocalStorage(itemName, initialValue = []) {
 	const [item, setItem] = useState(initialValue);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 	if (initialValue.length === 0) {
-		initialValue = [];
-	}
+    initialValue = defaultTodos;
+  }
 
 	useEffect(() => {
 		setTimeout(() => {
 			try {
-				const itemStored = localStorage.getItem(itemName);
+				let itemStored = localStorage.getItem(itemName);
 				let parsedItem;
 
-				if (JSON.parse(itemStored).length === 0) {
+				if (JSON.parse(itemStored).length === 0 || JSON.parse(itemStored).length === null) {
 					localStorage.setItem(
 						itemName,
 						JSON.stringify(initialValue)
