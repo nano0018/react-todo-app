92a8f1b186edce9425cca115419c1e5c3f3fb7f7
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-05-11 21:19:47 -0500

    :bug: Fixed issue related with default data on loading

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -4,11 +4,6 @@ function useLocalStorage(itemName, initialValue = []) {
   const [item, setItem] = useState(initialValue);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  if (initialValue.length === 0) {
-    initialValue = [
-      { text: 'Tomar el curso de intro a React', completed: false },
-    ];
-  }
 
   useEffect(() => {
     setTimeout(() => {
