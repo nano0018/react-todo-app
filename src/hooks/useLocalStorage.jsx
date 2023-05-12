b547825cda7dbc86122fe901d9ada8251f541806
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-05-11 21:26:22 -0500

    :bug: Fixed issue related with default data on loading

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,43 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function useLocalStorage(itemName, initialValue = []) {
-  const [item, setItem] = useState(initialValue);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+	const [item, setItem] = useState(initialValue);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	if (initialValue.length === 0) {
+		initialValue = [];
+	}
 
-  useEffect(() => {
-    setTimeout(() => {
-      try {
-        const itemStored = localStorage.getItem(itemName);
-        let parsedItem;
+	useEffect(() => {
+		setTimeout(() => {
+			try {
+				const itemStored = localStorage.getItem(itemName);
+				let parsedItem;
 
-        if (JSON.parse(itemStored).length === 0) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedItem = initialValue;
-        } else {
-          parsedItem = JSON.parse(itemStored);
-          setItem(parsedItem);
-        }
+				if (JSON.parse(itemStored).length === 0) {
+					localStorage.setItem(
+						itemName,
+						JSON.stringify(initialValue)
+					);
+					parsedItem = initialValue;
+				} else {
+					parsedItem = JSON.parse(itemStored);
+					setItem(parsedItem);
+				}
 
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error);
-      }
-    }, 1500);
-  }, [itemName, initialValue]);
+				setLoading(false);
+			} catch (error) {
+				setLoading(false);
+				setError(error);
+			}
+		}, 1500);
+	}, [itemName, initialValue]);
 
-  const modifyItem = (newItem) => {
-    setItem(newItem);
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-  };
+	const modifyItem = (newItem) => {
+		setItem(newItem);
+		localStorage.setItem(itemName, JSON.stringify(newItem));
+	};
 
-  return {
-    item,
-    modifyItem,
-    loading,
-    error,
-  };
+	return {
+		item,
+		modifyItem,
+		loading,
+		error,
+	};
 }
 
 export { useLocalStorage };
